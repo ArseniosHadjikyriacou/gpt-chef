0ba5c35fe8c18869d4b8b2106d7719ae747e2951
Arsenios Hadjikyriacou <arsenhad@gmail.com>
2025-02-02 21:26:02 +0200

    Fixed bug caused by Mistral returning markdown wrapped in triple quotes

--- src/components/GptRecipe.jsx
+++ src/components/GptRecipe.jsx
@@ -1,10 +1,13 @@
 import ReactMarkdown from "react-markdown"
 
 export default function GptRecipe(props) {
+  const lines = props.recipe.split('\n')
+  const filteredLines = lines.filter((line) => !line.includes('```'))
+  const recipe = filteredLines.join('\n');
   return (
       <section className="suggested-recipe-container">
         <h2>Chef Mistral Recommends:</h2>
-        <ReactMarkdown  children={props.recipe} />
+        <ReactMarkdown  children={recipe} />
       </section>
   )
 }
